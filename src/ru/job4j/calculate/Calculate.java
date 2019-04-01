5679b39f34c6d753f444df7b95ec73286e8f28e4
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-01 21:07:03 +0300

    1.1. Создание Hello World[#127313]

--- src/ru/job4j/calculate/Calculate.java
+++ src/ru/job4j/calculate/Calculate.java
@@ -3,7 +3,9 @@ package ru.job4j.calculate;
 public class Calculate { 
       
     public static void main (String args[]) {          
-        System.out.println("Hello World");
+         System.out.println("Hello World");
     }
 }
+
+
 
