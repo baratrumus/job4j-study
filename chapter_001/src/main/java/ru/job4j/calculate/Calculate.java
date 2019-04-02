15a459b3b72a017eeccf5e13b5cef28ef6ac215e
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-02 17:31:19 +0300

    Added JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс для вывода строки Hello World
+ * @author ivannikov
+ * @since 02.04.2019
+ * @version 1
+ */
+
 public class Calculate { 
-      
+
+	/**
+	 * Main
+	 * @param args - args.
+	 */
     public static void main (String args[]) {          
          System.out.println("Hello World");
     }
