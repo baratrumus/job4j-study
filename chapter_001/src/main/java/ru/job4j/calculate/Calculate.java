4b7feaaab9e0d1990924aff957670682ce601150
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-04 17:11:51 +0300

    IDEA files added to .gitignore

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,25 +7,26 @@ package ru.job4j.calculate;
  * @version 1
  */
 
-public class Calculate { 
+public class Calculate {
 
 	/**
 	 * Method echo.
 	 * @param name Your name.
 	 * @return Echo plus your name.
 	 */
-	 public String echo(String name) {
+	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
-	 }
+	}
+
 
 
 	/**
 	 * Main
 	 * @param args - args.
 	 */
-    public static void main(String[] args) {          
-         System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
 
 
