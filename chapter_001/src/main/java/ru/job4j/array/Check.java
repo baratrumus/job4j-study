372f62fa877b8a3a3f56682edb744a434af2a2aa
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-09 18:50:18 +0300

    Переворот массива

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * Class Класс должен проверить, что все элементы в массиве являются true или false.
+ * @author ivannikov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Check {
+
+    /**
+     *
+     * @param data входящий массив
+     * @return true or false
+     */
+
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        boolean tmp = data[0];
+        for (int i = 1; i < data.length; i++) {
+            if (tmp == data[i]) {
+                result = true;
+            } else {
+                return false;
+            }
+        }
+        return result;
+    }
+}
