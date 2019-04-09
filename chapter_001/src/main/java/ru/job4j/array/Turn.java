0553e51e7b2d09f20b50ad2d571adc58a0f393fc
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-09 18:24:26 +0300

    Переворот массива

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * Class Класс переворота массива
+ * @author ivannikov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Turn {
+    /**
+     *
+     * @param array входящий массив
+     * @return перевернутый массив
+     */
+    public int[] back(int[] array) {
+        int tmp;
+        for (int i = 0; i < array.length / 2; i++) {
+            tmp = array[array.length - i - 1];
+            array[array.length - i - 1] = array[i];
+            array[i] = tmp;
+
+        }
+        return array;
+    }
+}
+
