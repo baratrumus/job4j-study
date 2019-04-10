81693f8753b59521f510edd04bf741266cb826ae
Ilya Ivannikov <baratrumus@yandex.ru>
2019-04-10 17:01:33 +0300

    Задача 6.7 Матрица Рефакторинг

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,34 +5,8 @@ public class MatrixCheck {
     private final int size;
 
     public MatrixCheck(boolean[][] sourceMatrix) {
-        this.matrix = sourceMatrix;
+        matrix = sourceMatrix;
         size = sourceMatrix.length;
-    }
-
-    /**
-     * @return  true если все элементы диагонали лево верх - право низ  имеют одинаковое значение
-     */
-    private boolean leftToTopRight() {
-        boolean rst = true;
-        for (int i = 1; i < size; i++) {
-            if (matrix[size - i][i - 1] != matrix[size - i - 1][i]) {
-                rst = false;
-            }
-        }
-        return rst;
-    }
-
-    /**
-     * @return  true если все элементы диагонали слева снизу - право верх  имеют одинаковое значение
-     */
-    private boolean leftToBottomRight() {
-        boolean rst = true;
-        for (int i = 1; i < size; i++) {
-            if (matrix[i - 1][i - 1] != matrix[i][i]) {
-                rst = false;
-            }
-        }
-        return rst;
     }
 
     /**
@@ -42,9 +16,17 @@ public class MatrixCheck {
      *  например, одна диагональ - true, вторая-  false.
      */
     public boolean mono() {
-        return (size % 2 == 0) ? (leftToTopRight() || leftToBottomRight()) : (leftToTopRight() && leftToBottomRight());
-
+        boolean leftToTopRight = true;
+        boolean leftToBottomRight = true;
+        for (int i = 1; i < size; i++) {
+            if (matrix[size - i][i - 1] != matrix[size - i - 1][i]) {
+                leftToTopRight = false;
+            }
+            if (matrix[i - 1][i - 1] != matrix[i][i]) {
+                leftToBottomRight = false;
+            }
+        }
+        return (size % 2 == 0) ? (leftToTopRight || leftToBottomRight) : (leftToTopRight && leftToBottomRight);
     }
-
 
 }
