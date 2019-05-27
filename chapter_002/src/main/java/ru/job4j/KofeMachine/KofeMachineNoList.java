b509305cd5685619fc7f6fb63f1920635d7998d0
Ilya Ivannikov <baratrumus@yandex.ru>
2019-05-27 21:10:18 +0300

    Кофемашина без листа

---
+++ chapter_002/src/main/java/ru/job4j/KofeMachine/KofeMachineNoList.java
@@ -0,0 +1,35 @@
+package ru.job4j.KofeMachine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KofeMachineNoList {
+    private final int value;
+    private final int price;
+
+    KofeMachineNoList (int value, int price) {
+        this.value = value;
+        this.price = price;
+    }
+
+    public int[] changes1() {
+        int rest = this.value - this.price;
+        int[] rt = new int[2 * rest / 10 + 2];
+        int[] monets = new int[]{10, 5, 2, 1};
+        int i = 0;
+        for(int mon: monets) {
+            while (rest >= mon) {
+                rest -= mon;
+                rt[i] = mon;
+                i++;
+            }
+        }
+        return Arrays.copyOf(rt, i);
+    }
+
+    public static void main(String[] args) {
+        KofeMachineNoList km = new KofeMachineNoList(999, 22);
+        System.out.println(Arrays.toString(km.changes1()));
+    }
+}
--- chapter_002/src/test/java/ru/job4j/KofeMachine/KofeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/KofeMachine/KofeMachineTest.java
@@ -8,9 +8,17 @@ import static org.junit.Assert.assertThat;
 
 public class KofeMachineTest {
     @Test
-    public void when50and33TheRestIs5_5_5_2() {
+    public void when50and33TheRestIs10_5_2() {
         KofeMachine km = new KofeMachine(50, 33);
         assertThat(km.changes(), is(new int[]{10, 5, 2}));
+    }
+
+
+
+    @Test
+    public void when50and22() {
+        KofeMachineNoList km = new KofeMachineNoList(50, 22);
+        assertThat(km.changes1(), is(new int[]{10, 10, 5, 2, 1}));
     }
 }
 
