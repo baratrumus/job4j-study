56371a985a38a86c61cbaa62cd5066783ab21121
Ilya Ivannikov <baratrumus@yandex.ru>
2019-05-28 00:16:12 +0300

    Телефонный справочник на базе ArrayList.

---
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,36 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PhoneDictionary {
+
+    private List<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Телефонный справочник на базе ArrayList.
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        Iterator<Person> iter = persons.iterator();
+        Person per;
+        while(iter.hasNext()){
+            per = iter.next();
+            if (per.getName().contains(key) ||
+                    per.getSurname().contains(key) ||
+                    per.getPhone().contains(key) ||
+                    per.getAddress().contains(key)) {
+                result.add(per);
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+public void whenFindByName() {
+    PhoneDictionary phones = new PhoneDictionary();
+    phones.add(
+            new Person("Ivan", "Petrov", "334455", "Brest")
+    );
+    phones.add(
+            new Person("Oleg", "Krotov", "334455", "Brest")
+    );
+    phones.add(
+            new Person("Vasia", "Olgin", "334455", "Brest")
+    );
+    List<Person> persons = phones.find("Ol");
+    Iterator<Person> iter = persons.iterator();
+    assertThat(iter.next().getSurname(), is("Krotov"));
+    assertThat(iter.next().getSurname(), is("Olgin"));
+}
+}
