862032e3f374c499a077224dcf468e0d62260386
Ilya Ivannikov <baratrumus@yandex.ru>
2019-05-29 20:37:24 +0300

    Шахматы. Метод isDiagonal(). Рефакторинг.

--- chess/src/main/java/ru/job4j/chess/GetLineType.java
+++ chess/src/main/java/ru/job4j/chess/GetLineType.java
@@ -5,32 +5,11 @@ import ru.job4j.chess.firuges.Cell;
 public class GetLineType {
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        boolean ret = false;
-        int deltaX = 0;
-        int deltaY = 0;
-        int tmpX = source.x;
-        int tmpY = source.y;
-        if (source.x < dest.x) {
-            deltaX = 1;
-        } else if (source.x > dest.x) {
-            deltaX = -1;
-        } else {
-            return ret;
-        }
-        if (source.y < dest.y) {
-            deltaY = 1;
-        } else if (source.y > dest.y) {
-            deltaY = -1;
-        } else {
-            return ret;
-        }
-        for (int i = 0; i < 7; i++) {
-            tmpX += deltaX;
-            tmpY += deltaY;
-            if (tmpX == dest.x && tmpY == dest.y) {
-                ret = true;
-                break;
-            }
+        boolean  ret = false;
+        int deltaX = Math.abs(source.x - dest.x);
+        int deltaY = Math.abs(source.y - dest.y);
+        if (deltaX == deltaY) {
+            ret = true;
         }
         return ret;
     }
