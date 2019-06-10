a503ed25edc2206aa17b8239f17fab850aebbf71
Ilya Ivannikov <voldores@mail.ru>
2019-06-10 20:24:02 +0300

    Code review

---
+++ chapter_003/src/main/java/ru/job4j/codereview/Bank.java
@@ -0,0 +1,60 @@
+package ru.job4j.codereview;
+
+import ru.job4j.sorting.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+
+/**
+ * Не описан класс User в этом пакете.
+ * Т.е. не переопределен метод equals,  compareTo и TreeMap не отсортирует себя по ключу.
+ * Вообще не откомпилируется.  В остальном все вроде хорошо.
+ */
+
+public class Bank {
+
+    private TreeMap<User, ArrayList<Account>> treemap = new TreeMap<>();
+
+    public void addUser(User user) {
+        this.treemap.put(user, new ArrayList<>());
+    }
+
+    public void delete(User user) {
+        this.treemap.remove(user);
+    }
+
+    /**
+     * Здесь из this.treemap.get(user) мы получаем  ArrayList<>()
+     * @param user
+     * @param account
+     */
+    public void add(User user, Account account) {
+        this.treemap.get(user).add(account);
+    }
+
+    private Account getActualAccount(User user, Account account) {
+        ArrayList<Account> list = this.treemap.get(user);
+        return list.get(list.indexOf(account));
+    }
+
+    public void deleteAccount(User user, Account account) {
+        this.treemap.get(user).remove(account);
+    }
+
+    public List<Account> getAccounts(User user) {
+        return this.treemap.get(user);
+    }
+
+    public boolean transfer(User user1, Account account1,
+                                 User user2, Account account2, double amount) {
+        return this.treemap.get(user1).contains(account1)
+                && this.treemap.get(user2).contains(account2)
+                && getActualAccount(user1, account1).transfer(
+                getActualAccount(user2, account2), amount);
+    }
+
+    public String toString() {
+        return "Bank{" + "accounts=" + treemap + "}";
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/codereview/Convert.java
@@ -0,0 +1,47 @@
+package ru.job4j.codereview;
+
+
+import java.util.*;
+
+/**
+ * Непонятно зачем определили дефолтный конструктор, здесь он создался бы автоматически.
+ * Во втором методе есть лишние пустые строки.
+ * Комментарии вроде как лучше писать через джавадоки.
+ * В остальном вроде все ок.
+ */
+
+public class Convert {
+
+    public Convert(){
+
+    }
+
+    //Converts array to list
+    List<Integer> makeList(int[][] array) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++)
+                list.add(array[i][j]);
+        }
+        return list;
+    }
+
+
+    //Converts list to array
+    public int[][] makeArray(List<Integer> list, int rws) {
+        Iterator<Integer> iterator = list.iterator();
+        int cls = list.size() / rws + (list.size() % rws == 0 ? 0 : 1);
+
+
+        int[][] array = new int[rws][cls];
+        for (int i = 0; i < rws; i++) {
+            for (int j = 0; j < cls; j++) {
+                if (iterator.hasNext())
+                    array[i][j] = iterator.next();
+                else
+                    array[i][j] = 0;
+            }
+        }
+        return array;
+    }
+}
