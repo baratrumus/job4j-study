1b966fee865e02d5c6b5d946bc193a18bffb6ed3
Ilya Ivannikov <baratrumus@yandex.ru>
2019-10-31 15:46:42 +0300

    Создать меню - тесты

---
+++ chapter_008/src/test/java/isp/MenuTest.java
@@ -0,0 +1,62 @@
+package isp;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class MenuTest {
+    StartMenu sm = new StartMenu();
+
+
+    @Test
+    public void whenAddElementToRootItsAdded() {
+        String elNum = sm.menu.addElement("element 1", Optional.empty(),
+                "1", sm.new AddElement());
+        assertThat(elNum, is("1"));
+    }
+
+    @Test
+    public void whenAddElementToSubMenuItsAdded() {
+        String elNum = sm.menu.addElement("element 1", Optional.empty(),
+                "1", sm.new AddElement());
+        elNum = sm.menu.addElement("element 1.1", Optional.of(elNum),
+                "1", sm.new AddElement());
+
+        assertThat(elNum, is("1.1"));
+    }
+
+    @Test
+    public void whenAddElementToWrongParentItsNull() {
+        String elNum = sm.menu.addElement("element 1", Optional.of("1.1.1.1"),
+                "1", sm.new AddElement());
+        assertNull(elNum);
+    }
+
+    @Test
+    public void whenFindElementByInputKeyItsFound() {
+        String elNum = sm.menu.addElement("element 1", Optional.empty(),
+                "ttt", sm.new AddElement());
+        elNum = sm.menu.addElement("element 1.1", Optional.of(elNum),
+                "1", sm.new AddElement());
+        String key = sm.menu.findElementBy("ttt", "inputKey").get().getInputKey();
+
+        assertThat(key, is("ttt"));
+    }
+
+    @Test
+    public void whenFindElementByFullNumberItsFound() {
+        String elNum = sm.menu.addElement("element 1", Optional.empty(),
+                "1", sm.new AddElement());
+        elNum = sm.menu.addElement("element 1.1", Optional.of(elNum),
+                "2", sm.new AddElement());
+        String name = sm.menu.findElementBy(elNum, "fullNumber").get().getName();
+
+        assertThat(name, is("element 1.1"));
+    }
+
+
+}
