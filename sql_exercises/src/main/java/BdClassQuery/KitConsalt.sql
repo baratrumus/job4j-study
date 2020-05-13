cb2b700613b0aa5348118c348d95b7877bb29380
Ilya Ivannikov <voldores@mail.ru>
2020-05-13 16:31:29 +0300

    sql exercises update

--- sql_exercises/src/main/java/BdClassQuery/KitConsalt.sql
+++ sql_exercises/src/main/java/BdClassQuery/KitConsalt.sql
@@ -1,6 +1,26 @@
+
 --Дана таблица координат банкоматов с тремя колонками id, longtitude, latitude.
 -- Необходимо написать sql-запрос который находит ближайший идентификатор банкомата для заданных координат.
 -- Можно считать что координаты записаны в x0, y0 и к ним можно обращаться.
+
+
+-- Надо взять макс значение, которое меньше(слева) от Х.
+-- взять мин значение из тех что справа от X
+-- Вычесть из кажого сам Х, взять каждое по модулю.
+-- Наименьшее это ближ Х
+
+create table bankxy (
+                        id serial primary key,
+                        lon int,
+                        lat int
+);
+
+insert into bankxy(lon, lat) values (1, 3);
+insert into bankxy(lon, lat) values (2, 6);
+insert into bankxy(lon, lat) values (3, 1);
+insert into bankxy(lon, lat) values (6, 6);
+insert into bankxy(lon, lat) values (5, 1);
+
 
 --1 вариант
 select id, max(longtitude), max(latitude) from table as t
@@ -14,7 +34,6 @@ select id from table as t
 where t.longtitude > x0
 order by t.longtitude limit 1
 
-union select id from table as t
-where t.latitude > y0
-order by t.latitude limit 1;
-
+    union select id from table as t
+          where t.latitude > y0
+          order by t.latitude limit 1;
--- sql_exercises/src/main/java/BdClassQuery/selectByCompany.sql
+++ sql_exercises/src/main/java/BdClassQuery/selectByCompany.sql
@@ -1,3 +1,4 @@
+
 --есть две таблицы нужно
 
 --2) Select the name of the company with the maximum number of persons + number of persons in this company
@@ -36,8 +37,31 @@ insert into person (id, name, company_id) values (7, 'ivan', 5);
 select p.id, p.name, c.name from person as p left outer join company as c on c.id = p.company_id where c.id <> 5;
 
 --2) Select the name of the company with the maximum number of persons + number of persons in this company
-select count(p.company_id), c.name from person as p left outer join company as c on c.id = p.company_id group by c.name
-ORDER BY count(p.id) DESC LIMIT 1;
+select count(p.company_id), c.name from person as p left outer join company as c on c.id = p.company_id
+group by c.name ORDER BY count(p.id) DESC LIMIT 1;
+
+select tbl.nm, max(tbl.cnt) from
+    (select c.name as nm, count(c.id) as cnt from person as p left join company as c on c.id = p.company_id
+     group by c.id) as tbl
+
+
+
+--
+-- select top 1 t1.*
+-- from (select profit.source,count(*) as expr1
+--       from profit, have_d
+--       where profit.id = have_d.id
+--       group by profit.source) as t1
+-- order by expr1 desc
+--
+-- select t1.*
+--
+-- from (select profit.*,count(*) as expr1
+--       from profit, have_d
+--       where profit.id = have_d.id
+--       group by profit.id
+--       order by expr1 desc) as t1
+-- limit 1
 
 
 
